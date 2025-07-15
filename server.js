d59c3568b0a4f8e7d6d9d3d97ea814857005f460
shadowforce78 <planque.adam@gmail.com>
2025-07-15 17:47:04 +0200

    ✨ FEAT: Enhance module routing and add styles for form elements

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('modules', path.join(__dirname, 'modules', '*'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Modules/modulename/
+app.use('/modules', express.static(path.join(__dirname, 'modules')));
 
 function getModules() {
     return [
@@ -16,6 +16,24 @@ function getModules() {
 
 app.get('/', (req, res) => {
     res.render('index', { title: "BlackBox Toolkit - Accueil", modules: getModules() });
+});
+
+// app.get('/modules/notifmaker', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'modules', 'notifmaker', 'index.html'));
+// });
+
+// Route générique pour servir tous les modules
+app.get('/modules/:moduleName', (req, res) => {
+    const moduleName = req.params.moduleName;
+    const modulePath = path.join(__dirname, 'modules', moduleName, 'index.html');
+    
+    // Vérifier si le fichier existe
+    const fs = require('fs');
+    if (fs.existsSync(modulePath)) {
+        res.sendFile(modulePath);
+    } else {
+        res.status(404).send(`Module ${moduleName} non trouvé`);
+    }
 });
 
 const PORT = 3000;
