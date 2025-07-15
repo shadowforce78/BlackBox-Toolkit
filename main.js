b618bc19fc90cc0065f7fe50928c5e5924a32a4e
shadowforce78 <planque.adam@gmail.com>
2025-07-15 17:41:13 +0200

    ✨ FEAT: Add Notification Maker module and update server routing

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const { spawn } = require('child_process');
+const { app, BrowserWindow, Notification } = require("electron");
+const path = require("path");
+const { spawn } = require("child_process");
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: false
-    }
-  });
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+        },
+    });
 
-  // Lancer le serveur Express (localhost:3000)
-  const server = spawn('node', ['server.js']);
-  server.stdout.on('data', data => console.log(`[Server] ${data}`));
+    // Lancer le serveur Express (localhost:3000)
+    const server = spawn("node", ["server.js"]);
+    server.stdout.on("data", (data) => console.log(`[Server] ${data}`));
 
-  // Attendre un peu avant de charger
-  setTimeout(() => {
-    win.loadURL('http://localhost:3000');
-  }, 1000);
+    // Attendre un peu avant de charger
+    setTimeout(() => {
+        win.loadURL("http://localhost:3000");
+    }, 1000);
 }
 
 app.whenReady().then(createWindow);
--- server.js
+++ server.js
@@ -6,8 +6,16 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Modules/modulename/
+
+function getModules() {
+    return [
+        { name: "Notification Maker", path: "/modules/notifmaker" }
+    ];
+}
+
 app.get('/', (req, res) => {
-  res.render('index', { title: "BlackBox Toolkit" });
+    res.render('index', { title: "BlackBox Toolkit - Accueil", modules: getModules() });
 });
 
 const PORT = 3000;
